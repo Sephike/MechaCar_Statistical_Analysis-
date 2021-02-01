f8cf59140f25850ff02a00d7ad6d740fd8806d73
stephen hadley <stephenhadley20@hotmail.com>
2021-01-31 20:17:09 -0600

    Adding images and updating the MechaCar_challenge

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -3,11 +3,11 @@ library('ggplot2')
 library('jsonlite')
 library('dplyr')
 
-MechaCar_mpg <- read.csv(file = 'E:/R_stuff/Challenge/MechaCar_mpg.csv')
+MechaCar_mpg <- read_csv("Challenge/MechaCar_Statistical_Analysis-/MechaCar_mpg.csv")
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg))
 
-suspension_coil <- read.csv(file='E:/R_stuff/Challenge/Suspension_Coil.csv')
+suspension_coil <- read_csv("Challenge/MechaCar_Statistical_Analysis-/Suspension_Coil.csv")
 total_summary <- suspension_coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups='keep')
 
 lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups='keep')
