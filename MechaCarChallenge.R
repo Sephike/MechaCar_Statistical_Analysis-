2e628c1e7662c5f115e7b5b975f100bca08a6ae6
stephen hadley <stephenhadley20@hotmail.com>
2021-01-31 19:15:14 -0600

    Adding all files

---
+++ MechaCarChallenge.R
@@ -0,0 +1,21 @@
+library('tidyverse')
+library('ggplot2')
+library('jsonlite')
+library('dplyr')
+
+MechaCar_mpg <- read.csv(file = 'E:/R_stuff/Challenge/MechaCar_mpg.csv')
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg))
+
+suspension_coil <- read.csv(file='E:/R_stuff/Challenge/Suspension_Coil.csv')
+total_summary <- suspension_coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups='keep')
+
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups='keep')
+
+t.test(suspension_coil$PSI, mu=total_summary$Mean)
+
+t.test(subset(suspension_coil, Manufacturing_Lot == "Lot1")$PSI, mu=total_summary$Mean)
+
+t.test(subset(suspension_coil, Manufacturing_Lot == "Lot2")$PSI, mu=total_summary$Mean)
+
+t.test(subset(suspension_coil, Manufacturing_Lot == "Lot3")$PSI, mu=total_summary$Mean)
